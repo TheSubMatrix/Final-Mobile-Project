ce862f9366d6b46a884bf780b84e52150494568e
Colin Whiteford <colinwhiteford516@gmail.com>
2026-04-25 15:36:39 -0400

    Score Updating

--- Assets/Scripts/Project/Player/Player Score Handler.cs
+++ Assets/Scripts/Project/Player/Player Score Handler.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 public class PlayerScoreHandler : MonoBehaviour
 {
     [Inject] IScoreManager m_scoreManager;
+    Vector2 m_lastPosition;
+    void FixedUpdate()
+    {
+        if (!(Vector3.Distance(transform.position, m_lastPosition) > 1)) return;
+        m_lastPosition = transform.position;
+        m_scoreManager.AddScore(1);
+    }
 }
--- Assets/Scripts/Project/UI/PlayerHUD.cs
+++ Assets/Scripts/Project/UI/PlayerHUD.cs
@@ -10,4 +10,18 @@ public class PlayerHUD : MonoBehaviour, IDependencyProvider
     [SerializeField, RequiredField] TMP_Text m_scoreText;
     [SerializeField] string m_scorePrefix;
     [ClassSelector, SerializeReference] IScoreManager m_scoreManager;
+
+    void OnEnable()
+    {
+        m_scoreManager.Score.AddListener(OnScoreChanged);
+    }
+
+    void OnDisable()
+    {
+        m_scoreManager.Score.RemoveListener(OnScoreChanged);
+    }
+    void OnScoreChanged(uint score)
+    {
+        m_scoreText.text = m_scorePrefix + score;
+    }
 }
